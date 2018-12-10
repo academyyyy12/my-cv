c9f768fa7e445820483148637315db0598cf7ead
Jham <academyyyy12@gmail.com>
2018-12-10 20:12:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
     const person = {
       avatar: 'https://avatars2.githubusercontent.com/u/28197080?s=400&u=b72f8e7d02d0c33981bb5caea711c068ad70a5d8&v=4',
-      name: 'Muhammad Rifai',
+      name: 'Martin Jham',
       profession: 'FrontEnd - Backend Developer - Game Developer',
       bio: '❤ Member Developer Circles - Jakarta, Javascript Lover, Authors & activists of innovation. Better known as The MartinJham Developer.',
       address: 'Jl. Raya Cangkrang, Cikarawang, Bogor, Jawa Barat RT/RW 01/01',
@@ -32,6 +32,7 @@ class App extends Component {
       certificate: [
         {name: 'Learn How To Improve Your Testing with Artificial Intelligence | An Expert Roundup ', institution: 'Bar Kofman', date: 'Sep 25, 2018', description: 'Improve Testing Artificial Intelligence' },
         {name: 'Digital Innovation Contest Kota Bogor - Champion number 1', institution: 'Kasgoro', date: 'Agus 2018', description: 'High school / vocational level competition in Bogor city.' },
+        {name: 'Git: Become an Expert in Git & GitHub in 4 Hours', institution: 'Udemy', date: '2018', description: 'Udemy Course' },
         {name: 'Google I/O Extended 2017 Depok', institution: 'Google I/O', date: '2017', description: '' },
         {name: 'Game Development HTML 5', institution: 'dicoding', date: '26 Nov, 2017', description: '' },
         {name: 'LKS ASEAN - 2018 SEACC', institution: 'SEACC', date: '11 Aprl, 2018', description: '' },
@@ -40,7 +41,7 @@ class App extends Component {
         {name: 'Angular', percentage: '60%'},
         {name: 'SCSS', percentage: '90%'},
         {name: 'JavaScript', percentage: '90%'},
-        {name: 'PHP', percentage: '50%'},
+        {name: 'PHP', percentage: '35%'},
         {name: 'SWIFT', percentage: '30%'},
         {name: 'React', percentage: '80%'},
 
